c8cf5b1eea044ad22db6be2d5794ac1bc79865d2
Stijn Andeweg <57795390+Stijn-A@users.noreply.github.com>
2024-07-16 16:25:55 +0200

    remove paths to rivm dirs

--- 01_importeren_S_dropout.R
+++ 01_importeren_S_dropout.R
@@ -1,19 +1,17 @@
 
-source("/rivm/r/COVID-19/Surveillance/Scripts/01_Initialiseren/functies/functie_laad_data.R")
-
 data_teststraten_tmp <- 
-  functie_laad_data("/rivm/r/COVID-19/Surveillance/Data/Teststraten/Geschoond/", as.Date("2022-01-23"))  
+  functie_laad_data("Data/Teststraten/Geschoond/", as.Date("2022-01-23"))  
 # data van 20-12-2021 gebruikt voor Medrxiv
 # data van 23-01-2022 gebruikt voor eurosurv
 
 data_osiris_tmp <- 
-  functie_laad_data("/rivm/r/COVID-19/Surveillance/Data/OSIRIS/Geschoond/", as.Date("2022-01-23")) 
+  functie_laad_data("Data/OSIRIS/Geschoond/", as.Date("2022-01-23")) 
 # data van 20-12-2021 gebruikt voor Medrxiv
 # data van 23-01-2022 gebruikt voor eurosurv
 
 
 #oude data
-S_dropout_data_20211220 <- readRDS("/rivm/r/COVID-19/Surveillance/Scripts/Werkmappen/Stijn/VE_omicron_delta/data/S_dropout_data_20211220.rds")
+S_dropout_data_20211220 <- readRDS("Data/S_dropout_data_20211220.rds")
 
 data_S_dropout_org <- 
   "Data/Voor Stijn_20220121_V2.xlsx" %>% 
